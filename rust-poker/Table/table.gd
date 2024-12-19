fa600df4fd057d5e6e24b5b239e4d1c9ec86ea3e
ChrisBck <dopeyphuck@hotmail.com>
2024-12-19 20:19:48 +0800

    Community cards and hole cards can both be requested from the backed via http request. Best hand is also evaluated by backend http request.

--- rust-poker/Table/table.gd
+++ rust-poker/Table/table.gd
@@ -1,52 +1,95 @@
 extends Node
 
-@onready var card_1 = $HoleCard1
-@onready var card_2 = $HoleCard2
 @onready var http_request = $HTTPRequest
-@onready var hole_button = $HoleButton
+@onready var community_cards = $CommunityCards  # Container holding community Card nodes
+@onready var hole_cards = $HoleCards           # Container holding hole Card nodes
+@onready var evaluation_label = $Diagnostics/EvaluationLabel
 
-const SERVER_URL = "http://127.0.0.1:3030"  # Replace with your server's URL
-
+# Called when the node enters the scene tree for the first time
 func _ready():
-	pass
+	evaluation_label.text = "Ready to Play!"
 
+# Sends a request to deal hole cards
+func _on_HoleButton_pressed():
+	print("Requesting hole cards...")
+	var error = http_request.request("http://127.0.0.1:3030/deal_hole")
+	if error != OK:
+		print("HTTPRequest error: ", error)
 
-func _on_hole_button_pressed():
-	# Send a request to deal 2 cards
-	var url = SERVER_URL + "/deal?count=2"
-	http_request.request(url)
+# Sends a request to deal community cards
+func _on_CommunityButton_pressed():
+	print("Requesting community cards...")
+	var error = http_request.request("http://127.0.0.1:3030/deal_community")
+	if error != OK:
+		print("HTTPRequest error: ", error)
 
-
-func _on_http_request_request_completed(result, response_code, headers, body):
-	print("HTTP response received. Code:", response_code)
-	if response_code == 200:
-		var json_parser = JSON.new()  # Create an instance of the JSON class
-		var parse_result = json_parser.parse(body.get_string_from_utf8())
-
-		if parse_result == OK:
-			var response_data = json_parser.get_data()
-			print("Response:", response_data)
-			
-			# Handle "Deck has been reset" response (String)
-			if typeof(response_data) == TYPE_STRING and response_data == "Deck has been reset":
-				print("Deck reset successfully!")
-				# Optionally reset card visuals
-				card_1.animated_sprite.frame = 0
-				card_2.animated_sprite.frame = 0
-				
-			# Handle dealt cards response (Array)	
-			elif typeof(response_data) == TYPE_ARRAY and response_data.size() == 2:
-				# Handle deal response
-				card_1.set_card_from_json(response_data[0])
-				card_2.set_card_from_json(response_data[1])
-			else:
-				print("Unexpected response format:", response_data)
-		else:
-			print("Failed to parse JSON. Error code:", parse_result)
+# Sends a request to reset the game state
+func _on_ResetButton_pressed():
+	print("Resetting game state...")
+	var error = http_request.request("http://127.0.0.1:3030/reset")
+	if error == OK:
+		reset_all_cards()
+		evaluation_label.text = "Game Reset"
 	else:
-		print("Failed to fetch data. Response code:", response_code)
+		print("HTTPRequest error: ", error)
 
+# Handles HTTP request completion and processes the response
+func _on_HTTPRequest_request_completed(result, response_code, headers, body):
+	print("Request completed. Response code: ", response_code)
 
-func _on_reset_button_pressed():
-	var url = SERVER_URL + "/reset"
-	http_request.request(url)
+	if response_code == 200:
+		var json_parser = JSON.new()
+		var parse_result = json_parser.parse(body.get_string_from_utf8())
+		if parse_result == OK:
+			var data = json_parser.get_data()
+
+			# Extract cards and request type
+			var cards = data.get("cards", [])
+			var request_type = data.get("type", "")
+
+			if request_type == "hole":
+				print("Hole cards received: ", cards)
+				update_hole_cards(cards)  # Update hole cards
+			elif request_type == "community":
+				print("Community cards received: ", cards)
+				update_community_cards(cards)  # Update community cards
+			elif request_type == "evaluation":
+				var best_hand = data.get("best_hand", null)
+				print("Evaluation recieved")
+				update_besthand(best_hand)
+			else:
+				print("Unknown request type: ", request_type)
+		else:
+			print("Failed to parse JSON. Error code: ", parse_result)
+	else:
+		print("Request failed with response code: ", response_code)
+
+# Updates the card sprites with JSON data
+func update_card_sprites(card_sprites, card_data):
+	for i in range(len(card_data)):
+		if i < len(card_sprites):
+			var card = card_data[i]
+			card_sprites[i].set_card_from_json(card)
+
+# Updates the community cards
+func update_community_cards(card_data):
+	var card_sprites = community_cards.get_children()
+	update_card_sprites(card_sprites, card_data)
+	
+	var error = http_request.request("http://127.0.0.1:3030/evaluate")
+	if error != OK:
+		print("HTTPRequest error: ", error)
+		
+# Updates the hole cards
+func update_hole_cards(card_data):
+	var card_sprites = hole_cards.get_children()
+	update_card_sprites(card_sprites, card_data)
+
+func update_besthand(best_hand):
+	evaluation_label.text = "Best hand: " + best_hand
+	
+# Resets all card sprites to their default state
+func reset_all_cards():
+	var all_cards = community_cards.get_children() + hole_cards.get_children()
+	for card in all_cards:
+		card.set_card_from_json({"rank":"Two","suit":"Spades"})  # Reset to empty or default state
